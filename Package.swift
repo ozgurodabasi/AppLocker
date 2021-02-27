3ca736646b67d55ce315e11e99bfa1c3366207f7
Özgür Odabaşı <ozgurodabasi@gmail.com>
2021-02-27 17:28:18 +0300

    - Swift Package dependency updated

--- Package.swift
+++ Package.swift
@@ -25,9 +25,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AppLocker",
-            dependencies: []),
+            dependencies: ["Valet"]),
         .testTarget(
             name: "AppLockerTests",
-            dependencies: ["AppLocker"]),
+            dependencies: ["AppLocker", "Valet"]),
     ]
 )
